8a5023470dcbdd88ca0ca0ebf141b19dec1c17ec
Pashko Ostap <pashko.ostap@gmail.com>
2025-01-04 17:54:37 +0200

    added prisma schema

--- src/components/AddPost/AddPost.tsx
+++ src/components/AddPost/AddPost.tsx
@@ -1,44 +1,76 @@
+import { auth } from '@clerk/nextjs/server';
 import Image from 'next/image';
 
-const AddPost = () => (
-  <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'>
-    <Image
-      src='https://images.pexels.com/photos/23964497/pexels-photo-23964497/free-photo-of-portrait-of-a-man-wearing-a-black-suit.jpeg?auto=compress&cs=tinysrgb&w=200&dpr=2'
-      alt=''
-      width={40}
-      height={40}
-      className='w-10 h-10 rounded-full'
-    />
+import { prisma } from '@/lib/prismaClient';
 
-    <div className='flex-1'>
-      <div className='flex flex-1 items-end gap-4'>
-        <textarea
-          id=''
-          className='flex flex-1 p-2 min-h-16 bg-slate-100 rounded-lg'
-          placeholder='Type here...'
-        ></textarea>
-        <span className='flex w-5 h-5 cursor-pointer i-lucide-smile'></span>
-      </div>
-      <div className='flex flex-wrap items-center gap-4 mt-4 text-gray-400'>
-        <div className='flex items-center gap-2 cursor-pointer'>
-          <i className='i-lucide-file-image'></i>
-          Photo
+const AddPost = () => {
+  const formAction = async (formData: FormData) => {
+    'use server';
+    try {
+      const userId = 'user_2rAc1SuGhfJncg1v4PyaqQiW7v8';
+
+      const res = await prisma.post.create({
+        data: {
+          description: formData.get('description') as string,
+          userId,
+        },
+      });
+
+      console.log(res);
+    } catch (err) {
+      console.info(formData);
+      debugger;
+    }
+  };
+
+  return (
+    <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'>
+      <Image
+        src='https://images.pexels.com/photos/23964497/pexels-photo-23964497/free-photo-of-portrait-of-a-man-wearing-a-black-suit.jpeg?auto=compress&cs=tinysrgb&w=200&dpr=2'
+        alt=''
+        width={40}
+        height={40}
+        className='w-10 h-10 rounded-full'
+      />
+
+      <form className='flex-1' action={formAction}>
+        <div className='flex flex-1 items-end gap-4'>
+          <textarea
+            name='description'
+            id='description'
+            className='flex flex-1 p-2 min-h-16 bg-slate-100 rounded-lg'
+            placeholder='Type here...'
+          ></textarea>
+          <span className='flex w-5 h-5 cursor-pointer i-lucide-smile'></span>
         </div>
-        <div className='flex items-center gap-2 cursor-pointer'>
-          <i className='i-lucide-file-video'></i>
-          Video
+        <div className='flex flex-wrap items-center gap-4 mt-4 text-gray-400'>
+          <div className='flex items-center gap-2 cursor-pointer'>
+            <i className='i-lucide-file-image'></i>
+            Photo
+          </div>
+          <div className='flex items-center gap-2 cursor-pointer'>
+            <i className='i-lucide-file-video'></i>
+            Video
+          </div>
+          <div className='flex items-center gap-2 cursor-pointer'>
+            <i className='i-lucide-calendar-1'></i>
+            Event
+          </div>
+          <div className='flex items-center gap-2 cursor-pointer'>
+            <i className='i-lucide-vote'></i>
+            Poll
+          </div>
+
+          <button
+            className='bg-blue-500 text-white text-xs px-2 py-1 rounded-md ml-auto'
+            type='submit'
+          >
+            Save
+          </button>
         </div>
-        <div className='flex items-center gap-2 cursor-pointer'>
-          <i className='i-lucide-calendar-1'></i>
-          Event
-        </div>
-        <div className='flex items-center gap-2 cursor-pointer'>
-          <i className='i-lucide-vote'></i>
-          Poll
-        </div>
-      </div>
+      </form>
     </div>
-  </div>
-);
+  );
+};
 
 export default AddPost;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,79 +1,85 @@
 import Link from 'next/link';
 import MobileMenu from '../MobileMenu/MobileMenu';
 import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 
-const Navbar = () => (
-  <div className='h-24 flex items-center justify-between'>
-    {/* LEFT */}
-    <div className='md:hidden lg:block font-bold text-xl text-blue-600 w-[20%]'>
-      <Link href='/'>HOME</Link>
-    </div>
+const Navbar = () => {
+  // const data = auth();
 
-    {/* CENTER */}
-    <div className='hidden md:flex items-center justify-between font-bold text-gray-600 gap-6 w-[50%]'>
-      <Link href='/profile/id' className='flex gap-1 items-center'>
-        <span className='i-lucide-user' />
-        Profile
-      </Link>
-      <Link href='/profile/id' className='flex gap-1 items-center'>
-        <span className='i-lucide-user' />
-        Profile
-      </Link>
-      <Link href='/profile/id' className='flex gap-1 items-center'>
-        <span className='i-lucide-user' />
-        Profile
-      </Link>
-      <div className='hidden lg:flex p-2 bg-slate-100 items-center rounded-md'>
-        <input
-          type='text'
-          placeholder='Search...'
-          className='bg-transparent outline-none'
-        />
-        <span className='cursor-pointer i-lucide-x' />
+  // console.log(data.then(console.log));
+  return (
+    <div className='h-24 flex items-center justify-between'>
+      {/* LEFT */}
+      <div className='md:hidden lg:block font-bold text-xl text-blue-600 w-[20%]'>
+        <Link href='/'>HOME</Link>
+      </div>
+
+      {/* CENTER */}
+      <div className='hidden md:flex items-center justify-between font-bold text-gray-600 gap-6 w-[50%]'>
+        <Link href='/profile/id' className='flex gap-1 items-center'>
+          <span className='i-lucide-user' />
+          Profile
+        </Link>
+        <Link href='/profile/id' className='flex gap-1 items-center'>
+          <span className='i-lucide-user' />
+          Profile
+        </Link>
+        <Link href='/profile/id' className='flex gap-1 items-center'>
+          <span className='i-lucide-user' />
+          Profile
+        </Link>
+        <div className='hidden lg:flex p-2 bg-slate-100 items-center rounded-md'>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent outline-none'
+          />
+          <span className='cursor-pointer i-lucide-x' />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className='flex items-center justify-end gap-4 xl:gap-8 w-[30%]'>
+        <ClerkLoading>
+          <div
+            className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+            role='status'
+          >
+            <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
+              Loading...
+            </span>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className='cursor-pointer'>
+              <span className='i-lucide-users' />
+            </div>
+            <div className='cursor-pointer'>
+              <span className='i-lucide-mail' />
+            </div>
+            <div className='cursor-pointer'>
+              <span className='i-lucide-message-square' />
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <div className='hidden md:flex gap-4'>
+              <Link href='/sign-in' className='flex gap-1 items-center'>
+                <span className='i-lucide-user' />
+                Sign-in
+              </Link>
+              <Link href='/sign-up' className='flex gap-1 items-center'>
+                <span className='i-lucide-user' />
+                Sign-up
+              </Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
+
+        <MobileMenu />
       </div>
     </div>
-
-    {/* RIGHT */}
-    <div className='flex items-center justify-end gap-4 xl:gap-8 w-[30%]'>
-      <ClerkLoading>
-        <div
-          className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
-          role='status'
-        >
-          <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
-            Loading...
-          </span>
-        </div>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedIn>
-          <div className='cursor-pointer'>
-            <span className='i-lucide-users' />
-          </div>
-          <div className='cursor-pointer'>
-            <span className='i-lucide-mail' />
-          </div>
-          <div className='cursor-pointer'>
-            <span className='i-lucide-message-square' />
-          </div>
-        </SignedIn>
-        <SignedOut>
-          <div className='hidden md:flex gap-4'>
-            <Link href='/sign-in' className='flex gap-1 items-center'>
-              <span className='i-lucide-user' />
-              Sign-in
-            </Link>
-            <Link href='/sign-up' className='flex gap-1 items-center'>
-              <span className='i-lucide-user' />
-              Sign-up
-            </Link>
-          </div>
-        </SignedOut>
-      </ClerkLoaded>
-
-      <MobileMenu />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
